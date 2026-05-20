6eb62c4a71e064e35ac5075a8460e8d805fb7819
hhaerin95-ai <hhaerin95@gmail.com>
2026-05-20 19:36:35 +0800

    test no layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,1 @@
-@extends('layouts.app')
-
-@section('content')
 <h1>HOME PAGE OK</h1>
-@endsection
---
+++ storage/framework/views/cb97ddebfee9b4b4b142d9eda436b54b.php
@@ -0,0 +1,4 @@
+<?php $__env->startSection('content'); ?>
+<h1>HOME PAGE OK</h1>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH /var/www/resources/views/home.blade.php ENDPATH**/ ?>
